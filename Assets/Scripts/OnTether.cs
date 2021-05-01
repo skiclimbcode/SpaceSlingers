200feb35d58e3fc1eeb46512de93b855dbdb7ce4
Nathan Argall <Nathan.Argall@gmail.com>
2021-05-01 15:31:37 -0500

    change name of OnTether script

--- Assets/Scripts/OnTether.cs
+++ Assets/Scripts/OnTether.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class OnTether : MonoBehaviour
 {
     void Update ()
     {
@@ -18,5 +18,5 @@ public class Player : MonoBehaviour
             }
         }
     }
-    
+
 }
